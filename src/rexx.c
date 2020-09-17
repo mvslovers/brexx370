e998d04b5f8336ee02956fd84be1cdcbad755f74
Peter-Jacob <46247351+Peter-Jacob@users.noreply.github.com>
2020-09-17 11:34:40 +0200

    Internal REXX functions introduced

--- src/rexx.c
+++ src/rexx.c
@@ -128,7 +128,40 @@ RxFinalize( void )
     RxRegFunctionDone();	/* initialise register functions	*/
 } /* RxFinalize */
 
-/* ----------------- RxFileAlloc ------------------- */
+/* ----------------- RxPreLoaded ------------------- */
+int __CDECL
+RxPreLoaded(RxFile *rxf) {
+    if (strcasecmp(LSTR(rxf->name), "GETPOINTER") == 0) {
+        Lfx(&rxf->file, 64);
+        LZEROSTR(rxf->file);
+        strcpy(LSTR(rxf->file), "GETPOINTER: return c2d(storage(d2x(arg(1)),4))\0");
+      } else if (strcasecmp(LSTR(rxf->name), "CLRSCRN") == 0) {
+        Lfx(&rxf->file, 64);
+        LZEROSTR(rxf->file);
+        strcpy(LSTR(rxf->file), "CLRSCRN: ADDRESS TSO 'CLS';return 0\0");
+     }  else if (strcasecmp(LSTR(rxf->name), "STIME") == 0) {
+        Lfx(&rxf->file, 128);
+        LZEROSTR(rxf->file);
+        strcpy(LSTR(rxf->file),"STIME: PROCEDURE; PARSE VALUE TIME('L') WITH __HH':'__MM':'__SS'.'__HS;");
+        strcat(LSTR(rxf->file),"return __HH*360000+__MM*6000+__SS*100+__HS\0");
+    } else if (strcasecmp(LSTR(rxf->name), "B2C") == 0) {
+        Lfx(&rxf->file, 64);
+        LZEROSTR(rxf->file);
+        strcpy(LSTR(rxf->file), "B2C: return x2c(b2x(arg(1)))\0");
+    } else if (strcasecmp(LSTR(rxf->name), "C2B") == 0) {
+        Lfx(&rxf->file, 64);
+        LZEROSTR(rxf->file);
+        strcpy(LSTR(rxf->file), "C2B: return x2b(c2x(arg(1)))\0");
+    } else if (strcasecmp(LSTR(rxf->name), "ROOT") == 0) {
+        Lfx(&rxf->file, 64);
+        LZEROSTR(rxf->file);
+        strcpy(LSTR(rxf->file), "ROOT: return pow(arg(1),1/arg(2))\0");
+    } else return FALSE;
+    LLEN(rxf->file) = strlen(LSTR(rxf->file));
+    LTYPE(rxf->file) = LSTRING_TY;
+    LASCIIZ(rxf->file);
+    return TRUE;
+}
 RxFile* __CDECL
 RxFileAlloc(char *fname)
 {
@@ -203,7 +236,9 @@ RxFileLoad(RxFile *rxf, bool loadLibrary)
 
         /* try load via dsn */
         RxFileLoadDSN(rxf);
-    } else {								/* try to load a library */
+    } else {
+        if (RxPreLoaded(rxf)) return TRUE;     /* internal functions written in BREXX? */
+
         /* try to load from RXLIB */
         RxFileLoadDDN(rxf, "RXLIB");
         /* try to load from "ur" script location */
@@ -328,7 +363,6 @@ _LoadRexxLibrary(RxFile *rxf)
 
         RxInitCompile(rxf,NULL);
         RxCompile();
-
         /* restore state */
         MEMCPY(_error_trap,old_trap,sizeof(_error_trap));
         Rxcodestart = (CIPTYPE*)LSTR(*_code);
