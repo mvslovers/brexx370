cb3a7ccd803ebe8a4a67ff1cd77ee0fd2fe39288
Peter-Jacob <46247351+Peter-Jacob@users.noreply.github.com>
2020-10-03 17:55:32 +0200

    Integrated LINKMVS into BREXX

--- src/preload.c
+++ src/preload.c
@@ -19,7 +19,13 @@ RxPreLoad(RxFile *rxf,char *code) {
 int __CDECL
 RxPreLoaded(RxFile *rxf) {
     if (strcasecmp(LSTR(rxf->name), "PEEKA") == 0) {
-        RxPreLoad(rxf, "PEEKA: return c2d(storage(d2x(arg(1)),4))");
+            RxPreLoad(rxf, "PEEKA: return c2d(storage(d2x(arg(1)),4))");
+    }else if (strcasecmp(LSTR(rxf->name), "LINKMVS") == 0) {
+            RxPreLoad(rxf,"LINKMVS: Procedure;_p='';do pi=2 to arg();_p=_p''arg(pi)',';end;"
+                      "trace off;_a=address();ADDRESS SYSTEM;if arg()>1 then do;"
+                      "_p=substr(_p,1,length(_p)-1);interpret arg(1) \"'\"_p\"'\";end;"
+                      "else interpret arg(1);_r=rc;"
+                      "if _a<>'SYSTEM' then interpret \"ADDRESS \"_a;return _r");
     }else if (strcasecmp(LSTR(rxf->name), "BASE64DEC") == 0) {
             RxPreLoad(rxf,"BASE64DEC: procedure;trace off;"
                       "b64='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';"
@@ -28,18 +34,17 @@ RxPreLoaded(RxFile *rxf) {
                       "_s=_s||right(x2b(_o),6,0);end;return x2c(b2x(left(_s,length(_s)-2*length(_f))))");
     }else if (strcasecmp(LSTR(rxf->name), "BASE64ENC") == 0) {
             RxPreLoad(rxf,"BASE64ENC: Procedure;trace off;"
-                          "!b64='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';"
-                          "_f=x2b(c2x(arg(1)));_s='';_t=int((length(_f)/4)//3);"
-                          "_f=_f||copies('00',_t);do while _f<>'';parse var _f n 7 _f;"
-                          "n=x2d(b2x(n));_s=_s||substr(!b64,n+1,1);end;return _s||copies('=',_t );"
-                          ";");
-        } else if (strcasecmp(LSTR(rxf->name), "CLRSCRN") == 0) {
-        RxPreLoad(rxf, "CLRSCRN: ADDRESS TSO 'CLS';return 0");
+                       "!b64='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';"
+                       "_f=x2b(c2x(arg(1)));_s='';_t=int((length(_f)/4)//3);"
+                       "_f=_f||copies('00',_t);do while _f<>'';parse var _f n 7 _f;"
+                       "n=x2d(b2x(n));_s=_s||substr(!b64,n+1,1);end;return _s||copies('=',_t );");
+    } else if (strcasecmp(LSTR(rxf->name), "CLRSCRN") == 0) {
+            RxPreLoad(rxf, "CLRSCRN: ADDRESS TSO 'CLS';return 0");
     } else if (strcasecmp(LSTR(rxf->name), "STIME") == 0) {
-        RxPreLoad(rxf, "STIME: PROCEDURE; PARSE VALUE TIME('L') WITH __HH':'__MM':'__SS'.'__HS;"
+            RxPreLoad(rxf, "STIME: PROCEDURE; PARSE VALUE TIME('L') WITH __HH':'__MM':'__SS'.'__HS;"
                        "return __HH*360000+__MM*6000+__SS*100+__HS");
     }else if (strcasecmp(LSTR(rxf->name), "EPOCHTIME") == 0) {
-        RxPreLoad(rxf,"EPOCHTIME: procedure;trace off;"
+            RxPreLoad(rxf,"EPOCHTIME: procedure;trace off;"
                        "parse arg dd,mm,yy;if dd='' then do;"
                        "parse value date('e') with dd'/'mm'/'yy;yy='20'yy;"
                        "secs=time('s');end;else do;"
@@ -53,35 +58,35 @@ RxPreLoaded(RxFile *rxf) {
                        "if arg(1)<arg(2) | arg(1)>arg(3) then return 8;return 0;"
                        "tserror: SAY 'DATE IN ERROR: 'DD'.'MM'.'YY;exit 8");
     } else if (strcasecmp(LSTR(rxf->name), "MOD") == 0) {
-        RxPreLoad(rxf, "MOD: return arg(1)%arg(2)");
+           RxPreLoad(rxf, "MOD: return arg(1)%arg(2)");
     } else if (strcasecmp(LSTR(rxf->name), "REM") == 0) {
-        RxPreLoad(rxf, "REM: return arg(1)//arg(2)%1");
+           RxPreLoad(rxf, "REM: return arg(1)//arg(2)%1");
     } else if (strcasecmp(LSTR(rxf->name), "B2C") == 0) {
-        RxPreLoad(rxf, "B2C: return x2c(b2x(arg(1)))");
+           RxPreLoad(rxf, "B2C: return x2c(b2x(arg(1)))");
     } else if (strcasecmp(LSTR(rxf->name), "C2B") == 0) {
-        RxPreLoad(rxf, "C2B: return x2b(c2x(arg(1)))");
+           RxPreLoad(rxf, "C2B: return x2b(c2x(arg(1)))");
     } else if (strcasecmp(LSTR(rxf->name), "ROOT") == 0) {
-        RxPreLoad(rxf, "ROOT:;r=POW(arg(1),1/arg(2)); return r");
+           RxPreLoad(rxf, "ROOT:;r=POW(arg(1),1/arg(2)); return r");
     } else if (strcasecmp(LSTR(rxf->name), "DUMPVAR") == 0) {
-        RxPreLoad(rxf, "DUMPVAR: Procedure;trace off;parse upper arg vname;"
+           RxPreLoad(rxf, "DUMPVAR: Procedure;trace off;parse upper arg vname;"
                        "xname=value(vname,,0);vaddr=addr('xname');"
                        "vlen=length(xname);say 'Core Dump of 'vname', length 'vlen', length displayed 'vlen+16;"
                        "say copies('-',77);vlen=vlen+16;"
                        "call dumpit d2x(vaddr),vlen;return 0");
     }else if (strcasecmp(LSTR(rxf->name), "EPOCH2DATE") == 0) {
-            RxPreLoad(rxf,"EPOCH2DATE: procedure;trace off;"
+           RxPreLoad(rxf,"EPOCH2DATE: procedure;trace off;"
                        "udd=arg(1)%int(86400);udt=arg(1)//int(86400);"
                        "hh=right(udt%int(3600),2,'0');ut=udt//int(3600);"
                        "mm=right(ut%int(60),2,'0');ut=ut//int(60);ss=right(int(ut),2,'0');"
                        "return rxdate('e',udd,'unix')' 'hh':'mm':'ss");
     }else if (strcasecmp(LSTR(rxf->name), "GETTOKEN") == 0) {
-            RxPreLoad(rxf,"GETTOKEN: Procedure;trace off;"
+           RxPreLoad(rxf,"GETTOKEN: Procedure;trace off;"
                        "if abbrev('CENTURY',upper(arg(1)),1)=1 then do;"
                        "call wait 1;DX=date('sorted');DX=substr(DX,2,2)+365*substr(DX,4);"
                        "return DX||filter(time('L'),'.:');end;"
                        "return peeka(peeka(peeka(16)+604)+124)");
     }else if (strcasecmp(LSTR(rxf->name), "JOBINFO") == 0) {
-            RxPreLoad(rxf,"JOBINFO: procedure expose job.;trace off;drop job.;"
+           RxPreLoad(rxf,"JOBINFO: procedure expose job.;trace off;drop job.;"
                        "job.name=strip(peeks(__tiot(),8));ssib=peeka(__jscb()+316);"
                        "jobn=peeks(ssib+12,8);job.number=translate(jobn,'0',' ');"
                        "proc=strip(peeks(__tiot()+8,8));"
@@ -90,14 +95,14 @@ RxPreLoaded(RxFile *rxf) {
                        "return job.name;__tcb: return peeka(540);"
                        "__tiot: return peeka(__tcb()+12);__jscb: return peeka(__tcb()+180)");
     }else if (strcasecmp(LSTR(rxf->name), "PEEKS") == 0) {
-            RxPreLoad(rxf,"PEEKS: return storage(d2x(arg(1)),arg(2));");
+           RxPreLoad(rxf,"PEEKS: return storage(d2x(arg(1)),arg(2));");
     }else if (strcasecmp(LSTR(rxf->name), "VERSION") == 0) {
-            RxPreLoad(rxf,"VERSION: Procedure;parse upper arg mode;"
+           RxPreLoad(rxf,"VERSION: Procedure;parse upper arg mode;"
                        "parse version lang vers ptf '('datef')';"
                        "if abbrev('FULL',mode,1)=0 then return vers;parse var datef mm dd yy;"
                        "return 'Version 'vers' Build Date 'dd'. 'mm' 'yy");
     }else if (strcasecmp(LSTR(rxf->name), "SPLITBS") == 0) {
-        RxPreLoad(rxf,"SPLITBS:;trace off; parse arg I_#1,I_#2,I_#3;"
+          RxPreLoad(rxf,"SPLITBS:;trace off; parse arg I_#1,I_#2,I_#3;"
                       "if I_#2='' then I_#2='mystem.';"
                       "else if substr(I_#2,length(I_#2),1)<>'.' then I_#2=I_#2'.';"
                       "I_#3=upper(I_#3);I_#4=length(I_#3);interpret 'drop 'I_#2;I_7=1;I_5=1;"
