9c0746b12331bb8b80fc0e96b337c172fd992303
Mike Großmann <mike.grossmann.de@gmail.com>
2020-10-30 18:50:50 +0100

    ADDRESS LINKEXT removed

--- src/addrlink.c
+++ src/addrlink.c
@@ -57,19 +57,9 @@ handleLinkCommands(PLstr cmd, PLstr env)
     }
 
     if (rc == 0) {
-        int varCount;
+        int varCount = 0;
         int varValLen;
         int ii,jj;
-
-        // TEMP
-        struct evalblock *_evalblock_ptr = malloc(256 + sizeof(struct evalblock));
-        bzero(_evalblock_ptr, 256 + sizeof(struct evalblock));
-
-        _evalblock_ptr->evalblock_evpad1 = 0x00;
-        _evalblock_ptr->evalblock_evpad2 = 0x00;
-        _evalblock_ptr->evalblock_evlen  = 0x80000000;
-        _evalblock_ptr->evalblock_evsize = (256 + sizeof(struct evalblock)) / 8;
-        //
 
         strncpy(moduleName, loadModule, strlen(loadModule));
         linkParamsR15.moduleName = moduleName;
@@ -137,63 +127,10 @@ handleLinkCommands(PLstr cmd, PLstr env)
 
             linkParamsR1.ptr[ii - 1] = (void *) (((int)linkParamsR1.ptr[ii - 1]) | 0x80000000);
             svcParams.R1  = (unsigned int) &linkParamsR1;
-        } else if (strcasecmp((const char *)LSTR(*env), "LINKEXT") == 0) {
-            char *varNames[MAX_ARGS];
-            PLstr plsVarValue;
-
-            byte *tmp;
-            struct efpl _efpl;
-
-            char msg[10] = "BREXX/370";
-
-            struct argtable_entry argtableEntries[10];
-            memset(argtableEntries, 0xFF, sizeof(argtableEntries));
-
-            tmp = (byte *) &_efpl;
-
-            varCount = parse(args, varNames);
-
-            _efpl.efplcom  = 0;
-            _efpl.efplbarg = 0;
-            _efpl.efplearg = 0;
-            _efpl.efplfb   = 0;
-
-            // FIX FOR PL/1 => PL/1 is skipping 2 bytes for length field
-            tmp = tmp - 2;
-
-            _efpl.efplarg  = &argtableEntries;
-            _efpl.efpleval = &_evalblock_ptr;
-
-            for (ii = 0; ii < varCount; ii++) {
-
-                LPMALLOC(plsVarValue)
-
-                getVariable(varNames[ii], plsVarValue);
-                L2STR(plsVarValue);
-
-                argtableEntries[ii].argtable_argstring_ptr = LSTR(*plsVarValue);
-                argtableEntries[ii].argtable_argstring_length = LLEN(*plsVarValue);
-
-                FREE(plsVarValue);
-            }
-
-            linkParamsR1.ptr[0] = tmp;
-            linkParamsR1.ptr[0] = (void *) (((int)linkParamsR1.ptr[0]) | 0x80000000);
-            svcParams.R1  = (unsigned int) &linkParamsR1;
         }
 
         call_rxsvc(&svcParams);
         rc = svcParams.R15;
-
-        if (_evalblock_ptr->evalblock_evlen > 0) {
-            char result[256 + 1];
-            bzero(result, sizeof(result));
-
-            strncpy(result, (char *)&_evalblock_ptr->evalblock_evdata,
-                                     _evalblock_ptr->evalblock_evlen);
-
-            setVariable("RESULT", result);
-        }
 
         for (ii = 0; ii < varCount; ii++) {
             FREE(linkParamsR1.ptr[ii]);
