3b222836c9c13a297fae051bd420277219c778e8
Mike Großmann <mike.grossmann.de@gmail.com>
2020-11-02 12:22:17 +0100

    adding binary zero to the arguments

--- src/interpre.c
+++ src/interpre.c
@@ -528,6 +528,7 @@ I_CallFunction( void )
             for (i=nargs-1; i>=0; i--) {
                 if (existarg & bp) {
                     L2STR(RxStck[RxStckTop]);
+                    LASCIIZ(*RxStck[RxStckTop])
                     args[i] = (char *) LSTR(*RxStck[RxStckTop]);
                     RxStckTop--;
                 } else {
