828a3ee48c5a2676b71fa5309b4e03e9abcdc85b
Mike Großmann <mike.grossmann.de@gmail.com>
2020-12-09 09:44:52 +0100

    first implementation ob bare metal function library

---
+++ metal/metal.c
@@ -0,0 +1,246 @@
+#include <string.h>
+#include "metal.h"
+
+// TODO: implement get line size(tso macro) and cnewBuf(lineSize)
+static char line[80];
+static int  linePos = 0;
+
+void * _xregs    (unsigned int reg) {
+    void ** saveArea = GETSA();
+    saveArea = saveArea[1];
+
+    switch (reg) {
+        case  0:
+            return saveArea[5];
+        case  1:
+            return saveArea[6];
+        case  2:
+            return saveArea[7];
+        case  3:
+            return saveArea[8];
+        case  4:
+            return saveArea[9];
+        case  5:
+            return saveArea[10];
+        case  6:
+            return saveArea[11];
+        case  7:
+            return saveArea[12];
+        case  8:
+            return saveArea[13];
+        case  9:
+            return saveArea[14];
+        case 10:
+            return saveArea[15];
+        case 11:
+            return saveArea[16];
+        case 12:
+            return saveArea[17];
+        case 13:
+            return saveArea[18];
+        case 14:
+            return saveArea[3];
+        case 15:
+            return saveArea[4];
+            break;
+        default:
+            return NULL;
+    }
+}
+
+void   _tput     (char *data) {
+    int R0, R1, R15;
+
+    R0 = (int) strlen(data);
+    R1 = (int) (uintptr_t) data & 0x00FFFFFF;
+    R15 = 0;
+
+    SVC(93, &R0, &R1, &R15);
+}
+
+void   _putchar  (char character) {
+    line[linePos] = character;
+    linePos++;
+
+    if (character ==  '\n' || linePos == 80) {
+        _tput(line);
+        bzero(line, 80);
+        linePos = 0;
+    }
+}
+
+void   _clrbuf   (void) {
+    if (linePos > 0) {
+        _tput(line);
+        bzero(line, 80);
+        linePos = 0;
+    }
+}
+
+void * _getm  (size_t size) {
+    long *ptr;
+
+    int R0,R1,R15;
+
+    R0 = ((int)size) + AUX_MEM_HEADER_LENGTH;
+    R1 = -1;
+    R15 = 0;
+
+    SVC(10, &R0, &R1, &R15);
+
+    if (R15 == 0) {
+        ptr = (void *) (uintptr_t) R1;
+        ptr[0] = AUX_MEM_HEADER_ID;
+        ptr[1] = (((long) (ptr)) + AUX_MEM_HEADER_LENGTH);
+        ptr[2] = size;
+    } else {
+        ptr = NULL;
+    }
+
+    return (void *) (((uintptr_t) (ptr)) + AUX_MEM_HEADER_LENGTH);
+}
+
+void   _freem (void *ptr) {
+    int R0,R1,R15;
+
+    if (ptr != NULL) {
+        R0 = 0;
+        R1 = ((int) (uintptr_t) ptr) - AUX_MEM_HEADER_LENGTH;
+        R15 = -1;
+
+        SVC(10, &R0, &R1, &R15);
+    }
+}
+
+void * _malloc   (size_t size) {
+    return _getm(size);
+}
+
+void * _realloc  (void *oldPtr, size_t size) {
+    void *newPtr;
+
+    newPtr = _getm(size);
+
+    if (!newPtr) {
+        return NULL;
+    }
+
+    memcpy(newPtr, oldPtr, _msize(oldPtr));
+
+    _freem(oldPtr);
+
+    return newPtr;
+
+}
+
+void   _free     (void *ptr) {
+    // only call freemain for memory getmained by ourself
+    if (ptr != NULL && _ismetal(ptr)) {
+        _freem(ptr);
+    } else {
+        // TODO: maintain a list of orphaned pointers that must be freed by brexx
+    }
+}
+
+bool   _ismetal  (void *ptr) {
+    fword *tmp;
+
+    tmp = (fword *)((byte *)ptr - AUX_MEM_HEADER_LENGTH);
+
+    if (((fword) tmp) / MVS_PAGE_SIZE != ((fword) ptr) / MVS_PAGE_SIZE) {
+        return FALSE;
+    }
+
+    if (tmp[0] == AUX_MEM_HEADER_ID) {
+        if ( (void *)tmp[1] == ptr && tmp[2] > AUX_MEM_HEADER_LENGTH ) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    } else {
+        return FALSE;
+    }
+}
+
+size_t _msize    (void *ptr) {
+    size_t size = 0;
+
+    if (_ismetal(ptr)) {
+        fword *wrkPtr = (fword *) ((byte *) ptr - AUX_MEM_HEADER_LENGTH);
+        size = wrkPtr[2]; // 3rd dword contains the length
+    } else {
+        hword  *wrkPtr = (hword  *) ((byte *) ptr - JCC_MEM_HEADER_LENGTH);
+
+        // check if 1st dword is an address => jcc cell memory
+        if (*((fword *)wrkPtr) & 0xFFF) {
+
+            if (wrkPtr[3] >= 0 && wrkPtr[3] != 0xFFFF) {
+                size =  wrkPtr[3]; // 4rd word contains the length
+            }
+
+        } else {
+            //TODO: ???
+        }
+    }
+
+    return size;
+}
+
+void   _dump     (void *data, size_t size) {
+    char ascii[17];
+    size_t i, j;
+    bool padded = FALSE;
+
+    ascii[16] = '\0';
+
+    printf("%08X (+%08X) | ", (unsigned) (uintptr_t) data, 0);
+    for (i = 0; i < size; ++i) {
+        printf("%02X", ((char *)data)[i]);
+
+        if (isprint(((char *) data)[i])) {
+            ascii[i % 16] = ((char *)data)[i];
+        } else {
+            ascii[i % 16] = '.';
+        }
+
+
+        if ((i+1) % 4 == 0 || i+1 == size) {
+            if ((i+1) % 4 == 0) {
+                printf(" ");
+            }
+
+            if ((i+1) % 16 == 0) {
+                printf("| %s \n", ascii);
+                if (i+1 != size) {
+                    printf("%08X (+%08X) | ", (unsigned) (uintptr_t) &((char *)data)[i+1], (unsigned int) i+1);
+                }
+            } else if (i+1 == size) {
+                ascii[(i+1) % 16] = '\0';
+
+                for (j = (i+1) % 16; j < 16; ++j) {
+                    if ((j) % 4 == 0) {
+                        if (padded) {
+                            printf(" ");
+                        }
+                    }
+                    printf("  ");
+                    padded = TRUE;
+                }
+                printf(" | %s \n", ascii);
+            }
+        }
+    }
+}
+
+int    _upper    (int c)
+{
+    if(((c >= 'a') && (c <= 'i'))
+       ||((c >= 'j') && (c <= 'r'))
+       ||((c >= 's') && (c <= 'z')))
+    {
+        /* make uppercase */
+        c+=0x40;
+    }
+
+    return c;
+}
---
+++ metal/metal.h
@@ -0,0 +1,47 @@
+#ifndef __METAL_H
+#define __METAL_H
+
+#define FALSE     0
+#define TRUE      1
+
+#define AUX_MEM_HEADER_ID	  0xDEADBEAF
+
+#define AUX_MEM_HEADER_LENGTH 12
+#define JCC_MEM_HEADER_LENGTH 16
+#define MVS_PAGE_SIZE         4096
+#define printf printf_
+
+typedef int                 bool;
+typedef unsigned char       byte;
+typedef unsigned short      hword;
+typedef unsigned long       fword;
+typedef unsigned long long  dword;
+typedef unsigned long       uintptr_t;
+
+int __libc_arch = 0;
+
+// external assembler functions
+void * GETSA();                             // retunr pointer to current save area
+void   SVC(int svc,                         // call given svc
+           int *R0,
+           int *R1,
+           int *R15);
+
+// bare metal functions
+void  *_xregs    (unsigned int reg);           // return register value
+void   _tput     (char *data);                 // put text line to terminal
+void   _putchar  (char character);             // tbd
+void   _clrbuf   (void);                       // write outstanding data to terminal
+void  *_getm     (size_t size);                // get some main storage
+void   _freem    (void *ptr);                  // free some main storage
+bool   _ismetal  (void *ptr);                  // check if memory is "metal" memory
+size_t _msize    (void *ptr);                  // return length of allocated "metal" memory
+void  *_malloc   (size_t size);                // convenience function for __getm
+void  *_realloc  (void *oldPtr, size_t size);  // convenience function for __getm+memcpy+__freem
+void   _free     (void *ptr);                  // convenience function for __freem
+void   _dump     (void *data, size_t size);    // dump some memory in hex format
+int    _upper    (int c);                      // convert a single char to upper case
+bool   _isprint  (int c);                      // check if given char is printable
+
+
+#endif //__METAL_H
