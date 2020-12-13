99357eb09b82227e4b5bae1b0a50b13abcc43088
Mike Großmann <mike.grossmann.de@gmail.com>
2020-12-13 10:24:48 +0100

    new mvs support file added

---
+++ src/mvssup.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "mvssup.h"
+
+void   _dump     (void *data, size_t size, char *heading) {
+    char ascii[17];
+    size_t i, j;
+    int padded = 0;
+
+    ascii[16] = '\0';
+
+    if (heading != NULL) {
+        printf("[%s]\n", heading);
+    } else {
+        printf("[Dumping %lu bytes from address %p]\n", size, data);
+    }
+
+    printf("%08X (+%08X) | ", (unsigned) (uintptr_t) data, 0);
+    for (i = 0; i < size; ++i) {
+        printf("%02X", ((char *)data)[i]);
+
+        if (isprint(((char *) data)[i])) {
+            ascii[i % 16] = ((char *)data)[i];
+        } else {
+            ascii[i % 16] = '.';
+        }
+
+
+        if ((i+1) % 4 == 0 || i+1 == size) {
+            if ((i+1) % 4 == 0) {
+                printf(" ");
+            }
+
+            if ((i+1) % 16 == 0) {
+                printf("| %s \n", ascii);
+                if (i+1 != size) {
+                    printf("%08X (+%08X) | ", (unsigned) (uintptr_t) &((char *)data)[i+1], (unsigned int) i+1);
+                }
+            } else if (i+1 == size) {
+                ascii[(i+1) % 16] = '\0';
+
+                for (j = (i+1) % 16; j < 16; ++j) {
+                    if ((j) % 4 == 0) {
+                        if (padded) {
+                            printf(" ");
+                        }
+                    }
+                    printf("  ");
+                    padded = 1;
+                }
+                printf(" | %s \n", ascii);
+            }
+        }
+    }
+}
