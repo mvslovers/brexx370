e0b76761d45750e5fbc5cca07c49490df22ef96f
PeterJ <PeterJ88@t-online.de>
2021-03-21 19:08:34 +0100

    EXECIO  Added LIFO/FIFO parameter to receive file content

--- src/rxexecio.c
+++ src/rxexecio.c
@@ -6,20 +6,21 @@
 #include "hostenv.h"
 
 char *rxpull();
-void rxqueue(char *s);
+void rxqueue(char *s, int mode);
 long rxqueued();
 void remlf(char *s);
 
 int RxEXECIO(char **tokens) {
     int ii;
 
-    unsigned char pbuff[1025];
+    unsigned char pbuff[4098];
     unsigned char vname1[19];
     unsigned char vname2[19];
     unsigned char vname3[19];
-    unsigned char obuff[4097];
+    unsigned char obuff[4098];
     int ip1 = 0;
     int recs = 0;
+    int mode=2;      // 1: STEM, 2: FIFO queue, 3: LIFO queue
     FILE *f;
     PLstr plsValue;
 
@@ -28,28 +29,28 @@ int RxEXECIO(char **tokens) {
     // DISKR
     if (strcasecmp(tokens[2], "DISKR") == 0) {
         ip1 = findToken("STEM", tokens);
-        if (ip1 != -1) {
+        if (ip1 >= 0) {
+            mode = 1;
             ip1++;
             strcpy(vname1, tokens[ip1]);         // name of stem variable
-        }
+        } else if (findToken("FIFO", tokens) >= 0) mode = 2;
+          else if (findToken("LIFO", tokens) >= 0) mode = 3;
         f = fopen(tokens[3], "r");
         if (f == NULL) {
             LPFREE(plsValue)
             return 8;
         }
         recs = 0;
-        while (fgets(pbuff, 1024, f)) {
-            recs++;
-            remlf(&pbuff[0]);                     // remove linefeed
-            sprintf(vname2, "%s%d", vname1, recs);  // edited stem name
-            if (ip1 != -1) {
-                setVariable(vname2, pbuff);         // set rexx variable
-            }
-            if (ip1 == -1) {
-                rxqueue(pbuff);
-            }
+        while (fgets(pbuff, 4096, f)) {
+            remlf(&pbuff[0]); // remove linefeed
+            if (mode==1) {
+               recs++;
+               sprintf(vname2, "%s%d", vname1, recs);  // edited stem name
+               setVariable(vname2, pbuff);         // set rexx variable
+            } else if (mode==2) rxqueue(pbuff,1);
+              else rxqueue(pbuff,2);
         }
-        if (ip1 > 0) {
+        if (mode==1) {
             sprintf(vname2, "%s0", vname1);
             sprintf(vname3, "%d", recs);
             setVariable(vname2, vname3);
@@ -77,7 +78,7 @@ int RxEXECIO(char **tokens) {
         }
         if (ip1 == -1) {
             recs = rxqueued();
-        }
+         }
         for (ii = 1; ii <= recs; ii++) {
             if (ip1 != -1) {
                 memset(vname2, 0, sizeof(vname2));
@@ -87,9 +88,7 @@ int RxEXECIO(char **tokens) {
                 fputs(obuff, f);
             }
             if (ip1 == -1) {
-                memset(obuff, 0, sizeof(vname2));
-                sprintf(obuff, "%s\n", rxpull());
-                fputs(obuff, f);
+               fputs(rxpull(), f);
             }
         }
         fclose(f);
@@ -125,14 +124,15 @@ int RxEXECIO(char **tokens) {
 }
 
 void
-rxqueue(char *s)
+rxqueue(char *s,int mode)
 {
     PLstr pstr;
 
     LPMALLOC(pstr)
 
     Lscpy(pstr, s);
-    Queue2Stack(pstr);
+    if (mode==1) Queue2Stack(pstr);
+    else Push2Stack(pstr);
 }
 
 long
@@ -146,6 +146,9 @@ rxpull()
 {
     PLstr pstr;
     pstr = PullFromStack();
+    Lcat(pstr,"\n");
+    LASCIIZ(*pstr);
+
     return ((char *) LSTR(*pstr));
 }
 
