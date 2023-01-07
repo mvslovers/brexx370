9a88a37e7a160bb18421ece9cfeda6dee38e5fc4
mainframed <mainframed767@gmail.com>
2023-01-06 21:40:21 -0800

    that didnt work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,3 @@
-##
-## This config file is for use by Read The Docs
-##
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -42,6 +38,8 @@ with open("../../inc/rexx.h", 'r') as rexx_header:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [ 
+    'sphinx_rtd_theme',
+    'rinoh.frontend.sphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -58,10 +56,21 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# For PDF document formatting
+
+rinoh_documents = [dict(
+                        doc='index',                   # top-level file (index.rst)
+                        target='BREXX370_Users_Guide', # output file (BREXX370_Users_Guide.pdf)
+                        logo='_static/brexx370.png',   
+                        template='brexx.rtt',
+                        title='BREXX/370 User\'s Guide',
+                        subtitle='Release: {version}'.format(version=release),
+                       )
+                  ]   
